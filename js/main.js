3d32bdd900237908f18b9930aea1c3f76ed931ec
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-04-28 21:58:50 +0500

    Выполнил 1 задание

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
+    {id: 2, title: 'Mouse', price: 20, src: "img/mouse.jpg"},
+    {id: 3, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
+    {id: 4, title: 'Gamepad', price: 65, src: "img/gamepad.jpg"},
+    {id: 5, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
+    {id: 6, title: 'Mouse', src: "img/mouse.jpg"},
+    {id: 7, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
+    {id: 8, title: 'Gamepad', price: 65},
+];
+
+const renderProduct = (title, price = "Цена не определена", src = "img/no-picture.jpg") =>
+  `<div class="product-item">
+      <h3>${title}</h3>
+      <img src=${src} alt="товар" class="product-picture">
+      <p>${price}</p>
+      <button class="buy-btn">Купить</button>
+  </div>`;
+//Поскольку всё, что делает функция - это возвращает некоторое значение, можно убрать фигурные скобки и return
+
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item.title, item.price, item.src));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList.join("");
+};
+//Когда мы вставляем массив в innerHTML он преобразуется в строку по методу join. По умолчанию, в этом методе
+//вставляется разделитель - запятая. Можно самостоятельно установить разделитель - любой символ, пробел,
+// или даже пустую строку, то есть ничего.
+renderPage(products);
