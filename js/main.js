0be75255174a63ab0453382d810d6bf5903fd341
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-04-30 12:13:07 +0500

    Выполнил первые два задания

--- js/main.js
+++ js/main.js
@@ -1,31 +1,118 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
-    {id: 2, title: 'Mouse', price: 20, src: "img/mouse.jpg"},
-    {id: 3, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
-    {id: 4, title: 'Gamepad', price: 65, src: "img/gamepad.jpg"},
-    {id: 5, title: 'Notebook', price: 2000, src: "img/otebook.jpg"},
-    {id: 6, title: 'Mouse', src: "img/mouse.jpg"},
-    {id: 7, price: 35, src: "img/keyboard.jpg"},
-    {id: 8, title: 'Gamepad', price: 65},
-];
+class ProductsList {
+  constructor(container = '.products') {
+    this.products = [];
+    this.allProducts = [];
+    this.container = container;
+    this.totalPrice = 0;
+    this._init();
+  }
 
-const renderProduct = (title = "Без названия", price = "Цена не определена", src = "img/no-picture.jpg") =>
-  `<div class="product-item">
-      <h3>${title}</h3>
-      <img src=${src} alt=${title} class="product-picture" width="200" height="200">
-      <p>${price}</p>
-      <button class="buy-btn">Купить</button>
-  </div>`;
-//Поскольку всё, что делает функция - это возвращает некоторое значение, можно убрать фигурные скобки и return
+  _init() {
+    this._fetchProducts();
+    this._countTotalPrice();
+    this._render();
+  }
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price, item.src));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join("");
-};
-//Когда мы вставляем массив в innerHTML он преобразуется в строку по встроенному методу toString.
-// По умолчанию, в этом методе вставляется разделитель - запятая.
-// Можно самостоятельно установить метод Join и в нём разделитель - любой символ, пробел,
-// или даже пустую строку, то есть ничего.
-renderPage(products);
-//Сделал отдельный пулл-реквест, чтобы не было красно-зелёной зебры в изменённых файлах
+  _fetchProducts() {
+    this.products = [
+      {id: 1, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
+      {id: 2, title: 'Mouse', price: 20, src: "img/mouse.jpg"},
+      {id: 3, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
+      {id: 4, title: 'Gamepad', price: 65, src: "img/gamepad.jpg"},
+      {id: 5, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
+      {id: 6, title: 'Mouse', price: 20, src: "img/mouse.jpg"},
+      {id: 7, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
+      {id: 8, title: 'Gamepad', price: 65, src: "img/gamepad.jpg"},
+    ];
+  }
+
+  _render() {
+    const block = document.querySelector(this.container);
+    const el = document.querySelector('main');
+    for (let product of this.products) {
+      const prodObj = new ProductItem(product);
+      this.allProducts.push(prodObj);
+      block.insertAdjacentHTML('beforeend', prodObj.render());
+    }
+    el.insertAdjacentHTML('beforeend', `<h1>Общая цена равняется ${this.totalPrice}$</h1>`);
+  }
+
+  //Оставил этой функции единственную задачу - считать стоимость,
+  //а отображение на странице отправил в общий метод render
+  _countTotalPrice() {
+    for (let product of this.products) {
+      this.totalPrice += product.price;
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.src = product.src;
+  }
+
+  render() {
+    return `<div class="product-item">
+              <h3>${this.title}</h3>
+              <img src=${this.src} alt=${this.title} class="product-picture" width="200" height="200">
+              <p>${this.price}</p>
+              <button class="buy-btn">Купить</button>
+            </div>`;
+  }
+}
+
+const products = new ProductsList();
+
+/*
+class Cart {
+  constructor(container = '.cart') {
+    this.productsInCart = [];
+    this.container = container;
+  }
+
+  Необходимо получить массив товаров извне. Этот метод пусть вызывает сама корзина
+  _getProducts() {
+    this.productsInCart = [/!*Какой-то массив, собранный на странице*!/]
+  }
+
+  Подсчитать общую стоимость товаров. Пусть тоже работает автоматически
+  _countTotalPrice(){};
+
+  Отобразить всю корзину. Этот метод активируется извне, по кнопке "Показать корзину"
+  render(){};
+
+  иметь возможность очистить всю корзину
+  clearCart(){};
+
+  вызвать оформление заказа
+  initCheckout(){};
+}
+
+class CartItem {
+  constructor() {
+  this.properties=[];
+  }
+  Получить извне свойства товара(название, цена, картинка...)
+  _getProperties(){};
+
+  Удалить из корзины
+  deleteItem(){};
+
+  Добавить аналогичный
+  addMore(){};
+
+  добавить в избранное
+  addToFavorites(){};
+
+  выбрать опции(например расширенную гарантию)
+  setOptions(){};
+
+  показать наличие в магазинах(на карте)
+  showOnMap(){};
+}
+
+const products = new ProductsList();
+*/
