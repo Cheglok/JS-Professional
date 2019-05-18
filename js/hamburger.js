147430e5748c458d07a31a55f84a68cd07c0549c
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-05-18 21:51:29 +0500

    Доступные компоненты json

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,24 +1,60 @@
 class Hamburger {
-
+  constructor(container) {
+    this.container = container;
+    this.calories = 0;
+    this.price = 0;
+    this.components = [];
+    this.availibleComponents = {};
+    // {
+    //   big: {price: 100, calories: 40},
+    //   small: {price: 50, calories: 20},
+    //   cheese: {price: 10, calories: 20},
+    //   salad: {price: 20, calories: 5},
+    //   potato: {price: 15, calories: 10},
+    //   spice: {price: 15, calories: 0},
+    //   mayo: {price: 20, calories: 5},
+    // };
+    this._init();
 }
 
-class Hamburger {
-  constructor(size, stuffing) { ...
+  _init() {
+    const button = document.getElementById("construct");
+    button.addEventListener('click', () => this.construct());
+    this.construct()
   }
 
-  addTopping(topping) {
-    // Добавить добавку
-    }
-  removeTopping(topping);
-  { // Убрать добавку
+  construct() {
+    this.getComponents();
+    this.calculate();
+    this.render();
   }
-  getToppings(topping); {   // Получить список добавок }
-        getSize();
-        {              // Узнать размер гамбургера }
-          getStuffing();
-          {          // Узнать начинку гамбургера }
-            calculatePrice();
-            {       // Узнать цену }
-              calculateCalories();
-              {    // Узнать калорийность }
-              }
+
+  getComponents() {
+    this.components = [];
+    let checkedFields = [...document.querySelectorAll(`#${this.container} input:checked`)];
+    for (let field of checkedFields) {
+      let value = field.value;
+      this.components.push(value);
+    }
+    return fetch()
+
+
+
+    console.log(this.components);
+  }
+
+  calculate() {
+    this.price = 0;
+    this.calories = 0;
+    this.components.forEach(component => {
+      console.log(component);
+      this.price += this.availibleComponents[`${component}`].price;
+      this.calories += this.availibleComponents[component].calories;
+    });
+  }
+
+  render() {
+    document.querySelector('.result').textContent = `цена: ${this.price}, калорийность: ${this.calories}`;
+  }
+
+}
