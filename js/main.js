9bb3b1dd6675ba717b619d4d2ef2824bbac44561
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-05-22 18:58:30 +0500

    Start lesson 6 homework. copy previouse Vue code

--- js/main.js
+++ js/main.js
@@ -1,31 +1,94 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, src: "img/notebook.jpg"},
-    {id: 2, title: 'Mouse', price: 20, src: "img/mouse.jpg"},
-    {id: 3, title: 'Keyboard', price: 35, src: "img/keyboard.jpg"},
-    {id: 4, title: 'Gamepad', price: 65, src: "img/gamepad.jpg"},
-    {id: 5, title: 'Notebook', price: 2000, src: "img/otebook.jpg"},
-    {id: 6, title: 'Mouse', src: "img/mouse.jpg"},
-    {id: 7, price: 35, src: "img/keyboard.jpg"},
-    {id: 8, title: 'Gamepad', price: 65},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title = "Без названия", price = "Цена не определена", src = "img/no-picture.jpg") =>
-  `<div class="product-item">
-      <h3>${title}</h3>
-      <img src=${src} alt=${title} class="product-picture" width="200" height="200">
-      <p>${price}</p>
-      <button class="buy-btn">Купить</button>
-  </div>`;
-//Поскольку всё, что делает функция - это возвращает некоторое значение, можно убрать фигурные скобки и return
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price, item.src));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join("");
-};
-//Когда мы вставляем массив в innerHTML он преобразуется в строку по встроенному методу toString.
-// По умолчанию, в этом методе вставляется разделитель - запятая.
-// Можно самостоятельно установить метод Join и в нём разделитель - любой символ, пробел,
-// или даже пустую строку, то есть ничего.
-renderPage(products);
-//Сделал отдельный пулл-реквест, чтобы не было красно-зелёной зебры в изменённых файлах
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    basketUrl: '/getBasket.json',
+    products: [],
+    productsBasket: [],
+    filteredProducts: [],
+    imgCatalog: 'https://placehold.it/200x150',
+    imgBasket: 'https://placehold.it/50x50',
+    searchLine: "",
+    isVisibleCart: true,
+  },
+  mounted() {
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for (let el of data) {
+          this.products.push(el);
+        }
+      });
+    this.getJson(`getProducts.json`) /*Файлик расширил, чтобы можно было осуществить множественный поиск*/
+      .then(data => {
+        for (let el of data) {
+          this.products.push(el)
+        }
+      });
+    this.getJson(`${API + this.basketUrl}`)
+      .then(data => {
+        for (let el of data.contents) {
+          this.productsBasket.push(el);
+        }
+      })
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => console.log(error))
+    },
+    // Здесь использую три метода из предыдущей реализации, кажется что в них нет ничего лишнего
+    //
+    addProduct(product) {
+      this.getJson(`${API}/addToBasket.json`)
+        .then(data => {
+          if (data.result) {
+            let find = this.getItem(product.id_product);
+            if (find) {
+              find.quantity++;
+            } else {
+              let cartProd = Object.assign({quantity: 1}, product);
+              this.productsBasket.push(cartProd);
+            }
+          } else {
+            alert('Error!')
+          }
+        })
+    },
+    delProduct(product){
+      this.getJson(`${API}/deleteFromBasket.json`)
+        .then(data => {
+          if(data.result){
+            if(product.quantity > 1){
+              product.quantity--;
+            } else {
+              this.productsBasket.splice(this.productsBasket.indexOf(product), 1);
+            }
+          } else {
+            alert('Error!')
+          }
+        })
+    },
+    getItem(id){
+        return this.productsBasket.find(el => el.id_product === id)
+    },
+    filterGoods() {
+      this.filteredProducts = [];
+      let regExp = new RegExp( '^' + this.searchLine, 'i');
+      for (let el of this.products) {
+        //Поиск можно осуществлять по фразе любой длины, но обязательно с самого начала названия товара
+        //Хотел сделать чтобы с любого слова начинался поиск, но при '\\b' + this.searchLine, 'i'
+        // Перестаёт искать русские слова. Не считает русскую букву границей слова
+        if (el.product_name.match(regExp)) {
+          this.filteredProducts.push(el);
+        }
+      }
+    },
+    closeFiltered() {
+      this.filteredProducts = [];
+    },
+  }
+});
